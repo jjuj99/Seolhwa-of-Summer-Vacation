aab65afc4738946f1ed287569d1e995fd1d5fa24
jjuj99 <jjuj99@naver.com>
2022-07-16 18:52:46 +0900

    start

---
+++ main.lua
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- show default status bar (iOS)
+display.setStatusBar( display.DefaultStatusBar )
+
+-- include Corona's "widget" library
+local widget = require "widget"
+local composer = require "composer"
+
+
+-- event listeners for tab buttons:
+local function onFirstView( event )
+	composer.gotoScene( "view1" )
+end
+
+onFirstView()	-- invoke first tab button's onPress event manually
