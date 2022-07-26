4416f24fabcdab90f0fc98947e6b5ed19bbffc4c
kimseoyoungk <99073581+kimseoyoungk@users.noreply.github.com>
2022-07-26 20:56:10 +0900

    7.26 level1 확대 기능

--- game_simlang/level1.lua
+++ game_simlang/level1.lua
@@ -13,6 +13,7 @@ function scene:create( event )
 	-- 이미지 불러오기 ----
 	local background = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
 
+	local cardgroup= display.newGroup();
 	-- local back = display.newImageRect("image/p9_1.png",display.contentWidth, display.contentHeight)
 	-- back.x, back.y = display.contentWidth*0.5, display.contentHeight*0.5
 
@@ -34,20 +35,85 @@ function scene:create( event )
 	local board = display.newImageRect("image/보드.png",680,400)
 	board.x,board.y= display.contentWidth*0.5, display.contentHeight*0.48
 
+--------
 	local card1 = display.newImageRect("image/그림.png",300,180)
 	card1.x,card1.y= display.contentWidth*0.37, display.contentHeight*0.34
+	cardgroup[0]=card1
 
+	local card11 = display.newImageRect("image/그림2.png",300,180)
+	card11.x,card11.y= display.contentWidth*0.37, display.contentHeight*0.34
+	card11.alpha=0
+	cardgroup[1]=card11
+
+	local card12 = display.newImageRect("image/그림3.png",300,180)
+	card12.x,card12.y= display.contentWidth*0.37, display.contentHeight*0.34
+	card12.alpha=0
+	cardgroup[2]=card12
+
+	local card13 = display.newImageRect("image/그림4.png",300,180)
+	card13.x,card13.y= display.contentWidth*0.37, display.contentHeight*0.34
+	card13.alpha=0
+	cardgroup[3]=card13
+--------
 	local card2 = display.newImageRect("image/그림.png",300,180)
 	card2.x,card2.y= display.contentWidth*0.63, display.contentHeight*0.34
+	cardgroup[4]=card2
 
+	local card21 = display.newImageRect("image/그림2.png",300,180)
+	card21.x,card21.y= display.contentWidth*0.63, display.contentHeight*0.34
+	card21.alpha=0
+	cardgroup[5]=card21
+
+	local card22 = display.newImageRect("image/그림3.png",300,180)
+	card22.x,card22.y= display.contentWidth*0.63, display.contentHeight*0.34
+	card22.alpha=0
+	cardgroup[6]=card22
+
+	local card23 = display.newImageRect("image/그림4.png",300,180)
+	card23.x,card23.y= display.contentWidth*0.63, display.contentHeight*0.34
+	card23.alpha=0
+	cardgroup[7]=card23
+-------
 	local card3 = display.newImageRect("image/그림.png",300,180)
 	card3.x,card3.y= display.contentWidth*0.37, display.contentHeight*0.62
+	cardgroup[8]=card3
 
+	local card31 = display.newImageRect("image/그림2.png",300,180)
+	card31.x,card31.y= display.contentWidth*0.37, display.contentHeight*0.62
+	card31.alpha=0
+	cardgroup[9]=card31
+
+	local card32 = display.newImageRect("image/그림3.png",300,180)
+	card32.x,card32.y= display.contentWidth*0.37, display.contentHeight*0.62
+	card32.alpha=0
+	cardgroup[10]=card32
+
+	local card33 = display.newImageRect("image/그림4.png",300,180)
+	card33.x,card33.y= display.contentWidth*0.37, display.contentHeight*0.62
+	card33.alpha=0
+	cardgroup[11]=card33
+-------
 	local card4 = display.newImageRect("image/그림.png",300,180)
 	card4.x,card4.y= display.contentWidth*0.63, display.contentHeight*0.62
 
+	local card41 = display.newImageRect("image/그림2.png",300,180)
+	card41.x,card41.y= display.contentWidth*0.63, display.contentHeight*0.62
+	card41.alpha=0
+
+	local card42 = display.newImageRect("image/그림3.png",300,180)
+	card42.x,card42.y= display.contentWidth*0.63, display.contentHeight*0.62
+	card42.alpha=0
+
+	local card43 = display.newImageRect("image/그림4.png",300,180)
+	card43.x,card43.y= display.contentWidth*0.63, display.contentHeight*0.62
+	card43.alpha=0
+------
 	local level = display.newImageRect("image/단계.png",670,120)
 	level.x,level.y= display.contentWidth*0.5, display.contentHeight*0.89
+
+	local touchAn = display.newImage("image/숨은정답1.png")
+	touchAn.x,touchAn.y=700,500
+	touchAn:scale(0.06,0.06)
 
 	------이미지 불러오기 끝 ----------------
 
@@ -61,13 +127,88 @@ function scene:create( event )
 	sceneGroup:insert(item)
 	sceneGroup:insert(setting1)
 	sceneGroup:insert(board)
-	sceneGroup:insert(card1)
-	sceneGroup:insert(card2)
-	sceneGroup:insert(card3)
-	sceneGroup:insert(card4)
+	sceneGroup:insert(cardgroup)
 	sceneGroup:insert(level)
+	sceneGroup:insert(card4)
+	sceneGroup:insert(card41)
+	sceneGroup:insert(card42)
+	sceneGroup:insert(card43)
+	sceneGroup:insert(touchAn)
+	touchAn:toFront()
 	--레이어 정리 끝 -------------
 
+	--tap 확대 event------------------------------------------------
+	function card1:tap( event )
+		card1.alpha=0
+		card11.alpha=1
+	end
+	card1:addEventListener("tap", card1)
+	function card11:tap( event )
+		card11.alpha=0
+		card12.alpha=1
+	end
+	card11:addEventListener("tap", card11)
+
+	function card12:tap( event )
+		card12.alpha=0
+		card13.alpha=1
+	end
+	card12:addEventListener("tap", card12)
+---------------------
+	function card2:tap( event )
+		card2.alpha=0
+		card21.alpha=1
+	end
+	card2:addEventListener("tap", card2)
+	function card21:tap( event )
+		card21.alpha=0
+		card22.alpha=1
+	end
+	card21:addEventListener("tap", card21)
+	function card22:tap( event )
+		card22.alpha=0
+		card23.alpha=1
+	end
+	card22:addEventListener("tap", card22)
+--------------------
+	function card3:tap( event )
+		card3.alpha=0
+		card31.alpha=1
+	end
+	card3:addEventListener("tap", card3)
+	function card31:tap( event )
+		card31.alpha=0
+		card32.alpha=1
+	end
+	card31:addEventListener("tap", card31)
+	function card32:tap( event )
+		card32.alpha=0
+		card33.alpha=1
+	end
+	card32:addEventListener("tap", card32)
+----------------
+	function card4:tap( event )
+		card4.alpha=0
+		card41.alpha=1
+		touchAn.x,touchAn.y=800,450
+		touchAn:scale(1.3,1.3)
+	end
+	card4:addEventListener("tap", card4)
+	function card41:tap( event )
+		card41.alpha=0
+		card42.alpha=1
+		touchAn.x,touchAn.y=800,450
+		touchAn:scale(2,2)
+	end
+	card41:addEventListener("tap", card41)
+	function card42:tap( event )
+		card42.alpha=0
+		card43.alpha=1
+		touchAn.x,touchAn.y=800,450
+		touchAn:scale(3,3)
+	end
+	card42:addEventListener("tap", card42)
+--------------
 end
 
 function scene:show( event )
